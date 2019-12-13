4e0d7c4821ac30a3830f928d0b6d36ff4a812eaf
Flavio Dutra <flavio.adore@gmail.com>
2019-12-12 23:33:03 -0300

    Print e tipos

---
+++ types.py
@@ -0,0 +1,13 @@
+print('Hello', end=" ")
+print('world!')
+
+print('Hello', 'world', sep='-')
+
+print(11_111_111 + 1_111)
+print(0o123)
+print(0x291)
+print(3e8)
+print(-0.0000000000000001)
+
+print(True > False)
+print(True < False)
