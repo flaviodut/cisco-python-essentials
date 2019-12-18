f4ee152f5565de605275f47faca079eb31012d83
Flávio Dutra <flavio.adore@gmail.com>
2019-12-18 17:01:02 -0300

    Add simple conditionals example

---
+++ 4-conditionals.py
@@ -0,0 +1,16 @@
+# assigning 0 to var
+var = 0
+print(var != 0)
+
+# assigning 1 to var
+var = 1
+print(var != 0)
+
+# ex1
+n = int(input('Digite um numero: '))
+print(n >= 100)
+
+if n >= 100:
+    print('Epa')
+else:
+    print('Puxa')
