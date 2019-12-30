c8eb5805e977a686c969c7294c2cb03e603b86a5
Flávio Dutra <flavio.adore@gmail.com>
2019-12-30 16:35:22 -0300

    Adicionado operadores

---
+++ 12-operators.py
@@ -0,0 +1,18 @@
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a and not b)
+print(not (a and b))
+print(not a or not b)
+print(not (a or b))
+
+print('-')
+
+a = 0
+b = 1
+
+print(a & b)
+print(a | b)
+print(a ^ b)
